4b2b46f25946217f1e69aa69b2a195714e79dbbc
crealu <cameron.miller294@gmail.com>
2025-07-10 14:39:05 -0700

    Update server.js
    - Import viewArticles from routes/publish
    - Delete sendArticles function
    - Call viewArticles at /viewer endpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
 const { saveArticles } = require('./routes/publish');
 const { readScanned } = require('./routes/scan');
+const { viewArticles } = require('./routes/view');
 
 const app = express();
 const port = process.env.PORT || 7700;
@@ -11,22 +12,6 @@ dotenv.config({ path: '.env' });
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-async function sendArticles(response) {  
-  const uri = process.env.MONGO_URI;
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    const db = client.db('jetinfo');
-    const co = db.collection('articles');
-    const docs = await co.find().toArray();
-    response.send(docs);
-  } finally {
-    await client.close();
-    return null;
-	}
-}
 
 app.get('/', (req, res) => {
 	res.send('working');
@@ -53,7 +38,7 @@ app.get('/publisher', (req, res) => {
 });
 
 app.get('/viewer', (req, res) => {
-  sendArticles(res);
+  viewArticles(res);
 });
 
 app.post('/save-articles', (req, res) => {
