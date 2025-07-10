734af4bc1f615b74974c5ecc3ffaa672291f1751
crealu <cameron.miller294@gmail.com>
2025-07-10 14:29:53 -0700

    Create scan.js
    - Create route/scan.js to contain sendScanned function function

---
+++ routes/scan.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+
+module.exports = {
+  sendScanned: async (response) => {
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      const db = client.db('jetinfo');
+      const co = db.collection('scanned');
+      const docs = await co.find().toArray();
+      response.send(docs);
+    } finally {
+      await client.close();
+      return null;
+    }
+  }
+}
