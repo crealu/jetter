191f231548d808f62b2008e2669e42a1b2e43581
crealu <cameron.miller294@gmail.com>
2025-07-10 18:11:36 -0700

    Update server.js
    - Install and use cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const { saveArticles } = require('./routes/publish');
 const { readScanned } = require('./routes/scan');
@@ -12,6 +13,7 @@ dotenv.config({ path: '.env' });
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: './public'});
